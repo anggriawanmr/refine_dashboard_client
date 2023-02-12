2ca573cc3873399b7a050f3ec3600f9ae966ca0f
anggriawanmr <anggriawanmr@gmail.com>
2023-02-13 01:57:02 +0700

    Editing PieChart component :hammer:

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,7 +17,30 @@ const home = () => {
       </Typography>
 
       <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-        <PieChart />
+        <PieChart
+          title="Properties for Sale"
+          value={684}
+          series={[75, 25]}
+          colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart
+          title="Properties for Rent"
+          value={550}
+          series={[60, 40]}
+          colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart
+          title="Total Customers"
+          value={5684}
+          series={[75, 25]}
+          colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart
+          title="Properties for Cities"
+          value={555}
+          series={[75, 25]}
+          colors={['#275be8', '#c4e8ef']}
+        />
       </Box>
     </Box>
   );
