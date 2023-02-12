8a4786c5f08291a43cbf4ce14623e0ee399a1fb4
anggriawanmr <anggriawanmr@gmail.com>
2023-02-12 23:59:29 +0700

    Editing home page :hammer:

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,26 @@
 import { useList } from '@pankod/refine-core/dist/hooks/data/useList';
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+
+import {
+  PieChart,
+  PropertyReferrals,
+  PropertyCard,
+  TotalRevenue,
+  TopAgent,
+} from 'components';
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default home;
