0a9bbe9fbb1e6cec3da7ac834b11b6da7726a615
anggriawanmr <anggriawanmr@gmail.com>
2023-02-13 14:44:19 +0700

    Editing all properties page :hammer:

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { Add } from '@mui/icons-material';
+import { useList } from '@pankod/refine-core';
+import { Box, Stack, Typography, typography } from '@pankod/refine-mui';
+import { useNavigate } from '@pankod/refine-react-router-v6';
 
-const allProperties = () => {
-  return <div>allProperties</div>;
+import { PropertyCard, CustomButton } from 'components';
+
+const AllProperties = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography fontSize={25} fontWeight={700} color="#11142d">
+          <CustomButton
+            title="Add Property"
+            handleClick={() => {}}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            icon={<Add />}
+          />
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
-export default allProperties;
+export default AllProperties;
