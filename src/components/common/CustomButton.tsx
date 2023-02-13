4f22901691039d4e8c73a661bea1a4335ab5da48
anggriawanmr <anggriawanmr@gmail.com>
2023-02-13 14:54:05 +0700

    Editing CustomButton common component :hammer:

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import { Button } from '@pankod/refine-mui';
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+import { CustomButtonProps } from 'interfaces/common';
+
+const CustomButton = ({
+  type,
+  title,
+  backgroundColor,
+  color,
+  fullWidth,
+  icon,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <Button
+      sx={{
+        flex: fullWidth ? 1 : 'flex',
+        padding: '10px 15px',
+        width: fullWidth ? '100%' : 'fit-content',
+        minWidth: 130,
+        backgroundColor,
+        color,
+        fontSize: 16,
+        fontWeight: 600,
+        gap: '10px',
+        textTransform: 'capitalize',
+        '&:hover': {
+          opacity: 0.9,
+          backgroundColor,
+        },
+      }}
+      onClick={handleClick}
+    >
+      {icon} {title}
+    </Button>
+  );
 };
 
 export default CustomButton;
