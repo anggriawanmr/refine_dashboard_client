bb88b57bfe7247e136d7610d8df62c85dfdcfa3c
anggriawanmr <anggriawanmr@gmail.com>
2023-02-16 02:03:47 +0700

    Adding pagination feature :sparkles:

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -10,9 +10,11 @@ const CustomButton = ({
   fullWidth,
   icon,
   handleClick,
+  disabled,
 }: CustomButtonProps) => {
   return (
     <Button
+      disabled={disabled}
       type={type === 'submit' ? 'submit' : 'button'}
       sx={{
         flex: fullWidth ? 1 : 'flex',
--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -109,6 +109,50 @@ const AllProperties = () => {
           />
         ))}
       </Box>
+
+      {allProperties.length > 0 && (
+        <Box display="flex" gap={2} mt={3} flexWrap="wrap">
+          <CustomButton
+            title="Previous"
+            handleClick={() => setCurrent((prev) => prev - 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={!(current > 1)}
+          />
+          <Box
+            display={{ xs: 'hidden', sm: 'flex' }}
+            alignItems="center"
+            gap="5px"
+          >
+            Page{' '}
+            <strong>
+              {current} of {pageCount}
+            </strong>
+          </Box>
+          <CustomButton
+            title="Next"
+            handleClick={() => setCurrent((prev) => prev + 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={current === pageCount}
+          />
+          <Select
+            variant="outlined"
+            color="info"
+            displayEmpty
+            required
+            inputProps={{ 'aria-label': 'Without label' }}
+            defaultValue={10}
+            onChange={() => {}}
+          >
+            {[10, 20, 30, 40, 50].map((size) => (
+              <MenuItem key={size} value={size}>
+                Show {size}
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+      )}
     </Box>
   );
 };
