5f8d176ce6fd9c1e3c4bd146b6938dd46221cb6d
anggriawanmr <anggriawanmr@gmail.com>
2023-02-16 00:50:17 +0700

    Editing all properties page :hammer:

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,5 +1,5 @@
 import { Add } from '@mui/icons-material';
-import { useList } from '@pankod/refine-core';
+import { useTable } from '@pankod/refine-core';
 import { Box, Stack, Typography, typography } from '@pankod/refine-mui';
 import { useNavigate } from '@pankod/refine-react-router-v6';
 
@@ -7,6 +7,15 @@ import { PropertyCard, CustomButton } from 'components';
 
 const AllProperties = () => {
   const navigate = useNavigate();
+
+  const {
+    tableQueryResult: { data, isLoading, isError },
+  } = useTable();
+
+  const allProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Error...</Typography>;
 
   return (
     <Box>
@@ -21,6 +30,19 @@ const AllProperties = () => {
           />
         </Typography>
       </Stack>
+
+      <Box mt="20px" sx={{ display: 'flex', flexWrap: 'wrap', gap: 3 }}>
+        {allProperties.map((property) => (
+          <PropertyCard
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            price={property.price}
+            location={property.location}
+            photo={property.photo}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
