901c65d2f560f99fbd71e8efe7c5a2b3f7573716
anggriawanmr <anggriawanmr@gmail.com>
2023-02-16 08:09:25 +0700

    Editing Filter function :hammer:

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -9,6 +9,7 @@ import {
   MenuItem,
 } from '@pankod/refine-mui';
 import { useNavigate } from '@pankod/refine-react-router-v6';
+import { useMemo } from 'react';
 
 import { PropertyCard, CustomButton } from 'components';
 
@@ -28,6 +29,24 @@ const AllProperties = () => {
   } = useTable();
 
   const allProperties = data?.data ?? [];
+
+  const currentPrice = sorter.find((item) => item.field === 'price')?.order;
+
+  const toggleSort = (field: string) =>
+    setSorter([{ field, order: currentPrice === 'asc' ? 'desc' : 'asc' }]);
+
+  const currentFilterValues = useMemo(() => {
+    const logicalFilters = filters.flatMap((item) =>
+      'field' in item ? item : []
+    );
+
+    return {
+      title: logicalFilters.find((item) => item.field === 'title')?.value || '',
+      propertyType:
+        logicalFilters.find((item) => item.field === 'propertyType')?.value ||
+        '',
+    };
+  }, [filters]);
 
   if (isLoading) return <Typography>Loading...</Typography>;
   if (isError) return <Typography>Error...</Typography>;
@@ -56,8 +75,8 @@ const AllProperties = () => {
               mb={{ xs: '20px', sm: 0 }}
             >
               <CustomButton
-                title={`Sort price`}
-                handleClick={() => {}}
+                title={`Sort price ${currentPrice === 'asc' ? '↑' : '↓'}`}
+                handleClick={() => toggleSort('price')}
                 backgroundColor="#475be8"
                 color="#fcfcfc"
               />
@@ -65,8 +84,18 @@ const AllProperties = () => {
                 variant="outlined"
                 color="info"
                 placeholder="Search by title"
-                value=" "
-                onChange={() => {}}
+                value={currentFilterValues.title}
+                onChange={(e) => {
+                  setFilters([
+                    {
+                      field: 'title',
+                      operator: 'contains',
+                      value: e.currentTarget.value
+                        ? e.currentTarget.value
+                        : undefined,
+                    },
+                  ]);
+                }}
               />
               <Select
                 variant="outlined"
@@ -75,10 +104,35 @@ const AllProperties = () => {
                 required
                 inputProps={{ 'aria-label': 'Without label' }}
                 defaultValue=""
-                value=""
-                onChange={() => {}}
+                value={currentFilterValues.propertyType}
+                onChange={(e) => {
+                  setFilters(
+                    [
+                      {
+                        field: 'propertyType',
+                        operator: 'eq',
+                        value: e.target.value,
+                      },
+                    ],
+                    'replace'
+                  );
+                }}
               >
                 <MenuItem value="">All</MenuItem>
+                {[
+                  'Apartment',
+                  'Villa',
+                  'Farmhouse',
+                  'Condos',
+                  'Townhouse',
+                  'Duplex',
+                  'Studio',
+                  'Chalet',
+                ].map((type) => (
+                  <MenuItem key={type} value={type.toLowerCase()}>
+                    {type}
+                  </MenuItem>
+                ))}
               </Select>
             </Box>
           </Box>
