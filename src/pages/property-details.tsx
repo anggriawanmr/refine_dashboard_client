b6cdc67c487b3ef01f7a1c1710d9fa60ddba8a36
anggriawanmr <anggriawanmr@gmail.com>
2023-02-17 11:00:52 +0700

    Adding constant file in property details page :sparkles:

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -17,7 +17,31 @@ import {
 import { CustomButton } from 'components';
 
 const propertyDetails = () => {
-  return <div>propertyDetails</div>;
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const { id } = useParams();
+  const { mutate } = useDelete();
+  const { queryResult } = useShow();
+
+  const { data, isLoading, isError } = queryResult;
+
+  const propertyDetails = data?.data ?? {};
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
+  return (
+    <Box
+      borderRadius="15px"
+      padding="20px"
+      bgcolor="#fcfcfc"
+      width="fit-content"
+    >
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Details
+      </Typography>
+    </Box>
+  );
 };
 
 export default propertyDetails;
