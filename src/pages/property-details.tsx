119e3235a17e8ab2a8486278c73fffca51834fd8
anggriawanmr <anggriawanmr@gmail.com>
2023-02-17 10:52:48 +0700

    Adding importing files to property details page :sparkles:

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -1,4 +1,20 @@
-import React from 'react';
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+import {
+  useDelete,
+  useGetIdentity,
+  useShow,
+} from '@pankod/refine-core/dist/hooks';
+import { useParams, useNavigate } from '@pankod/refine-react-router-v6';
+import {
+  ChatBubble,
+  Delete,
+  Edit,
+  Phone,
+  Place,
+  Star,
+} from '@mui/icons-material';
+
+import { CustomButton } from 'components';
 
 const propertyDetails = () => {
   return <div>propertyDetails</div>;
