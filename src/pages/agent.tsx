b914645e311e45e041969e111af22cdcdfce7844
anggriawanmr <anggriawanmr@gmail.com>
2023-02-18 23:51:28 +0700

    Adding importing files and initial constant to agent page :sparkles:

--- src/pages/agent.tsx
+++ src/pages/agent.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import { useList } from '@pankod/refine-core';
+import { Typography, Box } from '@pankod/refine-mui';
+
+import { AgentCard } from 'components';
 
 const agent = () => {
+  const { data, isLoading, isError } = useList({
+    resource: 'users',
+  });
+
+  const allAgents = data?.data;
+
   return <div>agent</div>;
 };
 
