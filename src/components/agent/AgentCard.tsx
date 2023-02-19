19812a712ba3d0e3c7ca47f18e59202117f6de68
anggriawanmr <anggriawanmr@gmail.com>
2023-02-19 20:05:33 +0700

    Adding importing files to AgentCard component :sparkles:

--- src/components/agent/AgentCard.tsx
+++ src/components/agent/AgentCard.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { EmailOutlined, LocationCity, Phone, Place } from '@mui/icons-material';
+import { useGetIdentity } from '@pankod/refine-core/dist/hooks';
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+import { Link } from '@pankod/refine-react-router-v6';
 
-const AgentCard = () => {
+import { AgentCardProp, InfoBarProps } from 'interfaces/agent';
+
+const AgentCard = ({
+  id,
+  name,
+  email,
+  avatar,
+  noOfProperties,
+}: AgentCardProp) => {
   return <div>AgentCard</div>;
 };
 
