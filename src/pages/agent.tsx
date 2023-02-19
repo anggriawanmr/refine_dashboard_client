3ff3841663f3887af93fac5034a2198238d38be3
anggriawanmr <anggriawanmr@gmail.com>
2023-02-19 20:02:02 +0700

    Editing agent page :hammer:

--- src/pages/agent.tsx
+++ src/pages/agent.tsx
@@ -3,14 +3,44 @@ import { Typography, Box } from '@pankod/refine-mui';
 
 import { AgentCard } from 'components';
 
-const agent = () => {
+const Agent = () => {
   const { data, isLoading, isError } = useList({
     resource: 'users',
   });
 
-  const allAgents = data?.data;
+  const allAgents = data?.data ?? [];
 
-  return <div>agent</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Agents List
+      </Typography>
+
+      <Box
+        mt="20px"
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '20px',
+          backgroundColor: '#fcfcfc',
+        }}
+      >
+        {allAgents.map((agent) => (
+          <AgentCard
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
 };
 
-export default agent;
+export default Agent;
