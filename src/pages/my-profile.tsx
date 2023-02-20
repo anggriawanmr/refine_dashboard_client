0f89d881e9fcd6065eaec39a43179578bde53e0d
anggriawanmr <anggriawanmr@gmail.com>
2023-02-20 12:43:53 +0700

    Editing my profile page :hammer:

--- src/pages/my-profile.tsx
+++ src/pages/my-profile.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import { useGetIdentity, useOne } from '@pankod/refine-core';
+
+import { Profile } from 'components';
 
 const myProfile = () => {
+  const { data: user } = useGetIdentity();
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: user?.userid,
+  });
+
+  const myProfile = data?.data ?? [];
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+
   return <div>myProfile</div>;
 };
 
