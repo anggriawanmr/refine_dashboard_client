68975551d8bd430e92b39de292deee0c7ae0a056
anggriawanmr <anggriawanmr@gmail.com>
2023-02-20 12:37:24 +0700

    Adding InfoBar component inside AgentCard component :sparkles:

--- src/components/agent/AgentCard.tsx
+++ src/components/agent/AgentCard.tsx
@@ -5,6 +5,15 @@ import { Link } from '@pankod/refine-react-router-v6';
 
 import { AgentCardProp, InfoBarProps } from 'interfaces/agent';
 
+const InfoBar = ({ icon, name }: InfoBarProps) => (
+  <Stack flex={1} minWidth={{ xs: '100%', sm: 300 }} gap={1.5} direction="row">
+    {icon}
+    <Typography fontSize={14} color="#808191">
+      {name}
+    </Typography>
+  </Stack>
+);
+
 const AgentCard = ({
   id,
   name,
@@ -55,6 +64,27 @@ const AgentCard = ({
           <Typography fontSize={14} color="808191">
             Real-Estate Agent
           </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={2}
+        >
+          <InfoBar
+            icon={<EmailOutlined sx={{ color: '#808191' }} />}
+            name={email}
+          />
+          <InfoBar icon={<Place sx={{ color: '#808191' }} />} name="London" />
+          <InfoBar
+            icon={<Phone sx={{ color: '#808191' }} />}
+            name="+62856565123"
+          />
+          <InfoBar
+            icon={<LocationCity sx={{ color: '#808191' }} />}
+            name={`${noOfProperties} Properties`}
+          />
         </Stack>
       </Stack>
     </Box>
