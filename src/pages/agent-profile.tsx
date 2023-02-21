afb6db4f9c86df4cfc9f4715730578c0411e55e5
anggriawanmr <anggriawanmr@gmail.com>
2023-02-21 09:01:51 +0700

    Editing agent profile page :hammer:

--- src/pages/agent-profile.tsx
+++ src/pages/agent-profile.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { useOne } from '@pankod/refine-core';
+import { useParams } from '@pankod/refine-react-router-v6';
 
-const agentProfile = () => {
-  return <div>agentProfile</div>;
+import { Profile } from 'components';
+
+const AgentProfile = () => {
+  const { id } = useParams();
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: id as string,
+  });
+
+  const myProfile = data?.data ?? [];
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+
+  return (
+    <Profile
+      type="Agent"
+      name={myProfile.name}
+      email={myProfile.email}
+      avatar={myProfile.avatar}
+      properties={myProfile.allProperties}
+    />
+  );
 };
 
-export default agentProfile;
+export default AgentProfile;
